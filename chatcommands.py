00502c66966357a8912ff8a56c2524ea61b7a826
Sepehr Ahmadi <business@gocode.it>
2019-07-12 22:33:30 -0400

    First Commit

---
+++ chatcommands.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+from tconnection import TClientConnection, TRelayServer
+from tcommands import prepstr
+
+# this file re-creates the chat netmodule's commands, so that i dont have to pass them through to the server to get them.
+
+async def EmoteCommand(srv:TRelayServer,cl:TClientConnection,msg:str):
+    await srv.broadcastChat(cl,message="*"+cl.name+" "+msg,color=b"\x94\x4a\x00")
+
+async def PlayingCommand(srv:TRelayServer,cl:TClientConnection,msg:str):
+    msg = "Currently on the network: "+",".join([x.name for x in srv.conns])
+    msg += "\nOn the same server as you: "+",".join([x.name for x in srv.conns if cl.cur_server == x.cur_server])
+    await cl.sendchat(msg)
+
+async def RollCommand(srv:TRelayServer,cl:TClientConnection,msg:str):
+    from random import randint
+    await srv.broadcastChat(cl,message=cl.name+" has rolled a "+str(randint(0,100))+".",sendall=False)
+
+async def SayCommand(srv:TRelayServer,cl:TClientConnection,msg:str):
+    await srv.broadcastChat(cl,author=cl,message=msg)
+
+async def PartyCommand(srv:TRelayServer,cl:TClientConnection,msg:str):
+    await cl.prcon.writepkt(b"\x52\x01\x00"+prepstr("Party",1)+prepstr(msg,1)) # hacks!
+
+chatcommands = {
+    "Emote": EmoteCommand,
+    "Roll": RollCommand,
+    "Say": SayCommand,
+    "Party": PartyCommand,
+    "Playing": PlayingCommand
+}
---
+++ tconnection.py
@@ -0,0 +1,254 @@
+from __future__ import annotations
+import asyncio
+import struct
+
+real_server = ("10.1.0.199",7777)
+
+def prepstr(s:str,llen:int)->bytes:
+    stlen = len(s)
+    return stlen.to_bytes(llen,byteorder="big")+bytes(s,"utf-8")
+
+class TPlugin:
+    async def on_chat_message(self,cl,msg,sendall):
+        pass
+    async def on_connection(self,ip):
+        pass
+    async def on_server_join(self,server,cl):
+        pass
+    async def on_server_leave(self,server,cl):
+        pass
+    async def on_disconnect(self,cl):
+        pass
+
+class TRelayServer:
+    def __init__(self,listen_addr,loop=None):
+        self.host = listen_addr[0]
+        self.port = listen_addr[1]
+        self.conns = []
+        self.plugins = []
+        self.loop = loop or asyncio.get_event_loop()
+        self.server = loop.run_until_complete(asyncio.start_server(self.handle_terr,self.host,self.port,loop=loop))
+
+    async def handle_terr(self,inp,out):
+        cl = TClientConnection(inp,out,self)
+        await cl.set_server(real_server)
+        self.conns.append(cl)
+        asyncio.create_task(cl.listen())
+
+    async def close(self):
+        self.server.close()
+        await self.server.wait_closed()
+
+    async def broadcastChat(self,src:TClientConnection,author:TClientConnection=None,message:str="",color:bytes=None,sendall=True):
+        srvpkt = allpkt = b"\x52\x01\x00"
+        allpkt += b"\xff\x00"
+        if author is not None:
+            srvpkt += bytes(author.prmyslot) + b"\x00" + prepstr(message,2)
+            allpkt += prepstr("<"+author.name+">"+message,1)
+        else:
+            srvpkt += b"\xff\x00" + prepstr(message,1)
+            allpkt += prepstr(message,1)
+        
+        if color is None:
+            srvpkt += b"\xff\xff\xff"
+            allpkt += b"\xff\xff\xff"
+        else:
+            srvpkt += color
+            allpkt += color
+                
+        await self.broadcastPacket(srvpkt,lambda u:src.cur_server == u.cur_server)
+        if sendall and src is not None:
+            await self.broadcastPacket(allpkt,lambda u:src.cur_server != u.cur_server)
+    
+    async def broadcastPacket(self,pkt:bytes,cond):
+        for x in self.conns:
+            if cond(x): 
+                await x.writepkt(pkt)
+
+    async def handle_chat(self,client,chatcommand,message):
+        from chatcommands import chatcommands
+        if chatcommand == "Say" and message[0] == "-":
+            if await self.handle_command(client,message):
+                return
+        if chatcommand in chatcommands:
+            await chatcommands[chatcommand](self, client, message)
+
+
+    async def handle_command(self,client:TClientConnection,msg):
+        import tcommands
+        from importlib import reload
+        tcommands = reload(tcommands)
+        success = await tcommands.handle(msg,client,self)
+        return success
+
+    async def register_plugin(self,plugin:TPlugin):
+        self.plugins.append(plugin)
+
+class TerrariaConnection:
+    def __init__(self, rdr, wrtr):
+        self.reader,self.writer = rdr,wrtr
+
+    async def close(self):
+        self.writer.close()
+        await self.writer.wait_closed()
+
+    async def writepkt(self, d:bytes):
+        dl = (len(d)+2).to_bytes(2,byteorder="little")
+        self.writer.write(dl+d)
+
+    async def readpkt(self)->bytes:
+        i = await self.reader.readexactly(2)
+        i = int.from_bytes(i,byteorder="little")-2
+        return await self.reader.readexactly(i)
+
+    async def sendchat(self, msg:str, color:bytes=b"\xff\xff\xff"):
+        await self.writepkt(b"\x52\x01\x00\xff\x00"+prepstr(msg,1)+color)
+
+    async def listen(self):
+        while True:
+            try:
+                pkt = await self.readpkt()
+                await self.handle_pkt(pkt)
+            except asyncio.IncompleteReadError:
+                await self.close()
+                break
+            except Exception as e: # no other exception matters.
+                import traceback
+                traceback.print_exc()
+                continue
+
+    async def handle_pkt(self,pkt):
+        pass
+
+class TClientConnection(TerrariaConnection):
+
+    def __init__(self,rdr,wrtr,srv:TRelayServer):
+        self.initpkts = []
+        self.seen_npc_ids = []
+        self.seen_item_ids = []
+        self.ingame = False
+        self.prspawnpos = []
+        self.prmyslot = 0
+        self.respawn_on_world = False
+        self.cur_server = None
+        self.server = srv
+        self.name = ""
+        self.partycolor = None
+        TerrariaConnection.__init__(self,rdr,wrtr)
+
+    async def init_proxy(self,dest):
+        self.prcon = TProxyConnection(dest,self.handle_pr_pkt)
+        await self.prcon.connect()
+        asyncio.create_task(self.prcon.listen())
+        self.cur_server = dest
+
+    async def set_server(self,dst):
+        if self.ingame: # we are in a server already, 
+            if self.cur_server == dst: # if we're setting to the current server, just respawn.
+                self.respawn()
+                await self.sendchat("You're already connected to this server!",b"\xff\x22\x22")
+                return
+            
+            if self.prcon is not None:
+                oldprcon = self.prcon
+                await oldprcon.close() 
+                
+            await self.purge_data()
+            self.seen_npc_ids=[]
+            self.seen_item_ids=[]
+            
+            await self.init_proxy(dst)
+
+            for x in self.initpkts:
+                await self.prcon.writepkt(x)
+
+            self.respawn_on_world = True
+            await self.sendchat("Server changed.",b"\x22\xff\xff")
+        else: # this is the first server we're connecting to. no need to purge anything
+            await self.init_proxy(dst)
+
+    async def listen(self):
+        await TerrariaConnection.listen(self)
+
+    async def purge_data(self):
+        for x in self.seen_npc_ids:
+            await self.writepkt(b"\x17"+x+b"\x00"*40)
+        for x in self.seen_item_ids:
+            await self.writepkt(b"\x15"+x+b"\x00"*22)
+
+    async def respawn(self):
+        """ teleports the player to the spawn of the server they are currently connected to, does not ressurect or kill or revive or anything else, just a teleport """
+        await self.writepkt(b"\x0c"+bytes(self.prmyslot)+b"\x00"*8)
+
+    async def close(self):
+        await self.prcon.close()
+        await TerrariaConnection.close(self)
+
+    async def handle_pr_pkt(self,pkt):
+        if pkt[0] == 0x17:
+            self.seen_npc_ids.append(pkt[1:2])
+        elif pkt[0] == 0x15:
+            self.seen_item_ids.append(pkt[1:2])
+        elif pkt[0] == 0x07:
+            self.prspawnpos = pkt[16:24]
+            if self.respawn_on_world:
+                await self.respawn()
+        elif pkt[0] == 0x03:
+            print(pkt)
+            self.prmyslot = pkt[1]
+
+        if self.ingame and pkt[0] in [0x25]:
+            return
+
+        if pkt[0] == 0x0c:
+            print(pkt)
+
+        await self.writepkt(pkt)
+
+    async def handle_pkt(self,pkt):
+        if pkt[0] in [0x01,0x26,0x04,0x10,0x2a,0x32,0x05,0x06,0x08,0x0c]:
+            self.initpkts.append(pkt)
+
+        if pkt[0] == 0x0c and self.respawn_on_world:
+            print("!!")
+            await self.respawn()
+            self.respawn_on_world = False
+        
+        if not self.ingame and pkt[0] == 0x0c:
+            await self.sendchat("Welcome! This server uses TERRPROXY.\nType -help for a list of commands.",b"\x00\xff\xff")
+            self.ingame = True
+        if pkt[0] == 0x52: # load netmodule
+            u1,u2 = pkt[1:3]
+            tlen = pkt[3]
+            t = pkt[4:4+tlen].decode("utf-8")
+            try:
+                msglen = pkt[5+tlen]
+                msg = pkt[5+tlen:5+tlen+msglen].decode("utf-8")
+            except:
+                msglen = 0
+                msg = ""
+            if u1==0x01 and u2==0x00: # chat netmodule
+                await self.server.handle_chat(self,t,msg)
+                return
+        elif pkt[0] == 0x04:
+            nlen = int.from_bytes(pkt[4:5],byteorder="big")
+            self.name = pkt[5:5+nlen].decode("utf-8")
+        # elif pkt[0] == 0x2d:
+        #     p,t = pkt[0:1]
+        #     if p == self.prmyslot:
+        #         self.partycolor = ["\x"][t]
+
+        await self.prcon.writepkt(pkt)
+
+class TProxyConnection(TerrariaConnection):
+    def __init__(self, dest, on_pkt):
+        self.dest = dest
+        self.on_pkt = on_pkt
+        TerrariaConnection.__init__(self,None,None)
+
+    async def connect(self):
+        self.reader,self.writer = await asyncio.open_connection(self.dest[0],self.dest[1])
+
+    async def handle_pkt(self,pkt):
+        if self.on_pkt is not None:
+            await self.on_pkt(pkt)
