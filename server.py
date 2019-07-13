3b0f67518845fcb9a4646a59584e865c4b0ad7b7
Sepehr Ahmadi <business@gocode.it>
2019-07-12 22:39:42 -0400

    Remove dated TODO

--- server.py
+++ server.py
@@ -1,21 +1,3 @@
-# TODO some proxy functionality, acting as a middleman between 2 servers 
-#  (just fwd packets at first, including chat, but capture all SAY "/"s as commands)
-#  See if we can switch between servers mid-game 
-#   Crude idea: send completely new zone packets and send faux disconnect packets
-#   Better version: see if any of the server-inits clear the client cache and fuck with that. 
-#        Looks like when you send 0x25 midgame on a server with a password, the client goes 
-#          back to loading screen, maybe this can be abused? Have not tested if it is a working
-#          loading screen or not. Might be worth a shot to send 0x25 and all future packets
-#          and then see if there's any single packet that causes a cache flush 
-#          (assuming there is a cache....).
-#  
-# The best place to start the remote connection would most likely be when the client sends a x01,
-#  since we can just forward it to the other server and forward that servers x03 back (or x25/x02)
-#
-# TODO figure out why the client keeps timing out (being a full proxy will most likely fix this)
-# 
-
-
 import asyncio
 import struct
 from terrelay.tconnection import TerrariaConnection, TClientConnection, TRelayServer
